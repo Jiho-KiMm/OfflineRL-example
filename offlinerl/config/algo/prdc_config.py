f89fa3596e638df32ffc75be77311bc11f89d99d
yi <songyigao@gmail.com>
2024-03-29 17:40:13 +0800

    Add search parameter range for the PRDC algorithm

--- offlinerl/config/algo/prdc_config.py
+++ offlinerl/config/algo/prdc_config.py
@@ -14,7 +14,6 @@ max_epoch = 1000
 batch_size = 256
 state_dim = None
 action_dim = None
-device = 'cpu'
 alpha = 2.5
 beta = 2.0
 k = 1
@@ -27,3 +26,10 @@ expl_noise = 0.1
 critic_lr = 3e-4
 actor_lr = 3e-4
 max_action = 1.0
+
+
+#tune
+grid_tune = {
+    "alpha" : [2.5, 7.5, 20.0, 40.0],
+    "beta" : [2.0, 7.5, 15.0],
+}
